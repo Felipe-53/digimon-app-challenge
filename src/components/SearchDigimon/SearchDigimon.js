9c5d905d11ed229a8e640ee3efdf36cd2bbcd840
Felipe53 <felipeandresb97@gmail.com>
2021-03-23 11:21:31 -0300

    error handling

--- src/components/SearchDigimon/SearchDigimon.js
+++ src/components/SearchDigimon/SearchDigimon.js
@@ -2,16 +2,41 @@ import React, { useState, useEffect } from 'react'
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Filters from './Filters/Filters';
 import useStyles from './searchDigimonStyles';
-
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import { BadApiResponseError } from '../../utils/errors';
 
 function SearchDigimon({ openModal, set_openModal, searchType, set_searchType, searchValue, set_searchValue, fetchData, set_whichPage }) {
   const classes = useStyles();
+
+  const [searchStatus, set_searchStatus] = useState('notInitiated');
+
+  const mapping = {
+    'notInitiated': (
+      <Filters
+        searchType={searchType}
+        searchValue={searchValue}
+        set_searchValue={set_searchValue}
+        changeFilter={changeFilter}
+      />
+    ),
+
+    'loading': (
+      <CircularProgress />
+    ),
+
+    'badRequestError': (
+      <h3>The Digimon you searched doesn't exist</h3>
+    ),
+
+    'unexpectedError': (
+      <h3>An unexpected error happened :(</h3>
+    )
+  }
 
   function changeFilter(event) {
     set_searchValue(null);
@@ -19,58 +44,67 @@ function SearchDigimon({ openModal, set_openModal, searchType, set_searchType, s
   }
 
   function handleSearchClick() {
-    fetchData().then(data => {
-      if (data instanceof BadApiResponseError) {
-        return // error handling here
-      }
-      set_openModal(false);
-      if (data.length > 1) {
-        set_whichPage('listing');
-      }
-      set_searchType('name');
-      set_searchValue(null);
-    })
+    set_searchStatus('loading');
+
+    fetchData()
+      .then(data => {
+        set_openModal(false);
+        if (data.length > 1) {
+          set_whichPage('listing');
+        }
+        set_searchType('name');
+        set_searchValue(null);
+      })
+      .catch(err => {
+        if (err instanceof BadApiResponseError) {
+          return set_searchStatus('badRequestError');
+        }
+
+        set_searchStatus('unexpectedError');
+      })
   }
 
   function handleClose() {
     set_openModal(false);
     set_searchType('name');
     set_searchValue(null);
+    setTimeout(() => set_searchStatus('notInitiated'), 100);
+  }
+
+  function actions() {
+    if (searchStatus === 'notInitiated') {
+      return (
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSearchClick}
+          >
+            SEARCH
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+          >
+            SERCH ALL
+          </Button>
+        </DialogActions>
+      );
+    }
+
+    return null;
   }
 
   return (
     <Dialog PaperProps={{style: {width: '60%'}}} open={openModal} onClose={handleClose}>
       <DialogTitle>Search Digimon</DialogTitle>
       <DialogContent className={classes.dialogContent}>
-        <Filters
-          searchType={searchType}
-          searchValue={searchValue}
-          set_searchValue={set_searchValue}
-          changeFilter={changeFilter}
-        />
+        {mapping[searchStatus]}
       </DialogContent>
 
-      <DialogActions>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSearchClick}
-        >
-          SEARCH
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-        >
-          SERCH ALL
-        </Button>
-      </DialogActions>
+      {actions()}
     </Dialog>
   );
 }
 
-
-
 export default SearchDigimon;
-
-
